43f6efe4f90ffb6c2018107ba96dcfa276871f14
alex1261bat <alex126.1bat@gmail.com>
2023-08-25 11:38:54 +0300

    feat: реализовать ДЗ "Context и Intent" и "EditText и TextWatcher"

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,53 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<androidx.appcompat.widget.Toolbar>(R.id.search_back_button)
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+        }
+
+        fun clearButtonVisibility(s: CharSequence?): Int {
+            return if (s.isNullOrEmpty()) {
+                View.GONE
+            } else {
+                View.VISIBLE
+            }
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+
+        searchEditText.addTextChangedListener(searchTextWatcher)
+
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -8,10 +11,38 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backArrowButton = findViewById<androidx.appcompat.widget.Toolbar>(R.id.back_arrow)
+        val backButton = findViewById<androidx.appcompat.widget.Toolbar>(R.id.settings_back_button)
+        val shareButton = findViewById<FrameLayout>(R.id.share_button)
+        val supportButton = findViewById<FrameLayout>(R.id.sendToSupport_button)
+        val userAgreementButton = findViewById<FrameLayout>(R.id.userAgreement_button)
 
-        backArrowButton.setOnClickListener {
+        backButton.setOnClickListener {
             finish()
+        }
+
+        shareButton.setOnClickListener {
+            val sendIntent = Intent(Intent.ACTION_SEND)
+            val message = "https://practicum.yandex.ru/android-developer/"
+            sendIntent.putExtra(Intent.EXTRA_TEXT, message)
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+
+        supportButton.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("alex126.1bat@gmail.com"))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT,
+                "Тема: Сообщение разработчикам и разработчицам приложения Playlist Maker")
+            startActivity(supportIntent)
+        }
+
+        userAgreementButton.setOnClickListener {
+            val userAgreementIntent = Intent(Intent.ACTION_VIEW)
+            userAgreementIntent.data = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            startActivity(userAgreementIntent)
         }
     }
 }
