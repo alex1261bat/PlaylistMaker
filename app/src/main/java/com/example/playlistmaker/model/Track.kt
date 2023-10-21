704b7eee275da7d3d6544e8874df12fb128cbfb6
alex1261bat <alex126.1bat@gmail.com>
2023-10-21 22:08:59 +0300

    feat: реализовать Спринт №13

--- app/src/main/java/com/example/playlistmaker/model/Track.kt
+++ app/src/main/java/com/example/playlistmaker/model/Track.kt
@@ -3,9 +3,13 @@ package com.example.playlistmaker.model
 import com.google.gson.annotations.SerializedName
 
 data class Track(
-    val trackId: Int,
+    val trackId: String,
     val trackName: String,
     val artistName: String,
     @SerializedName("trackTimeMillis") val trackTime: String,
-    val artworkUrl100: String
+    val artworkUrl100: String,
+    val collectionName: String?,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String
 )
